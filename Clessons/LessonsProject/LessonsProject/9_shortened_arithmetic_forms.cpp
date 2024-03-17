be386ff05111819453a28ab1d1a977ffb91982c0
LORAN <vinana222@gmail.com>
2024-03-17 15:08:25 +0300

    8-10 lessons

---
+++ Clessons/LessonsProject/LessonsProject/9_shortened_arithmetic_forms.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// сокращенные арифметические формы 
+
+int main()
+{
+	int a = 0;
+
+	a += 20; // a = a + 20;
+
+	a -= 20; // a = a - 20;
+
+	a *= 20; // a = a * 20;
+
+	a /= 20; // a = a / 20;
+
+	a %= 20; // a = a % 20;
+
+	return 0;
+}
