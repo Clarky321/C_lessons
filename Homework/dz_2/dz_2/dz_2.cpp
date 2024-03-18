4c5ba11f6e450603d5ce612d13badadca4fcfd9e
LORAN <vinana222@gmail.com>
2024-03-18 12:00:14 +0300

    dz_2

---
+++ Homework/dz_2/dz_2/dz_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+// Написать программу, проверяющую число, введенное с клавиатуры на четность.
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int var;
+
+    cout << "Введите число для проверки на чётность: " << endl;
+    cin >> var;
+
+    if (var % 2 == 0)
+    {
+        cout << "Число " << var << " чётное" << endl;
+    }
+    else
+    {
+        cout << "Число " << var << " не чётное" << endl;
+    }
+
+    return 0;
+}
