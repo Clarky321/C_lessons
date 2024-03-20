e522ed50d7a0e53cdf8b82277072eb890cdbe519
LORAN <vinana222@gmail.com>
2024-03-20 09:21:28 +0300

    dz_3

---
+++ Clessons/LessonsProject/LessonsProject/13_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Понятие цикла.
+// Цикл while.
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+
+
+	return 0;
+}
---
+++ Homework/dz_3/dz_3/dz_3.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a, b, selection;
+    cout << "Введите 2 числа" << endl;
+    cin >> a >> b;
+
+    cout << "Выберите математическую операцию" << endl;
+    
+    cout << "1. Сложение" << endl;
+    cout << "2. Вычитание" << endl;
+    cout << "3. Умножение" << endl;
+    cout << "4. Деление" << endl;
+
+    cin >> selection;
+
+    switch (selection)
+    {
+    case 1:
+    {
+        cout << "Результат сложения: " << a + b << endl;
+    }
+    break;
+
+    case 2:
+    {
+        cout << "Результат вычитания: " << a - b << endl;
+    }
+    break;
+
+    case 3:
+    {
+        cout << "Результат умножения: " << a * b << endl;
+    }
+    break;
+
+    case 4:
+    {
+        if (b != 0)
+        {
+            cout << "Результат деления: " << (double)a / b << endl;
+        }
+        else
+        {
+            cout << "На ноль делить нельзя" << endl;
+        }
+    }
+    break;
+
+    default:
+        cout << "Ошибка! Пункт меню отсутствует!" << endl;
+        break;
+    }
+
+    return 0;
+}
