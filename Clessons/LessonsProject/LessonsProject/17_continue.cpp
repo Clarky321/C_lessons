6b28075415948a2b7441095730e548425d2243a3
LORAN <vinana222@gmail.com>
2024-03-22 09:07:39 +0300

    17-18

---
+++ Clessons/LessonsProject/LessonsProject/17_continue.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// ключевое слово continue
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 5)
+		{
+			continue; // при ключевом слове continue, происходит пропуск условия (i = 5)
+		}
+
+		cout << "переменная i = " << i << endl;
+	}
+
+	return 0;
+}
---
+++ Clessons/LessonsProject/LessonsProject/18_nested_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	for (i = 1; i < 5; i++)
+	{
+		cout << "Сработал 1-й цикл for итерация № " << i << endl;
+	}
+
+	return 0;
+}
