167593f7dbb30b5eb3b9c30e09087958c763d644
LORAN <vinana222@gmail.com>
2024-03-22 10:04:19 +0300

    fix

--- Clessons/LessonsProject/LessonsProject/18_nested_loop.cpp
+++ Clessons/LessonsProject/LessonsProject/18_nested_loop.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// вложенные циклы
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
@@ -8,6 +10,11 @@ int main()
 	for (i = 1; i < 5; i++)
 	{
 		cout << "Сработал 1-й цикл for итерация № " << i << endl;
+
+		for (int j = 1; j < 5; j++)
+		{
+			cout << "\tсработал 2й цикл for итерация № " << j << endl;
+		}
 	}
 
 	return 0;
