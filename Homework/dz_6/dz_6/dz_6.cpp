a23f0042aeb384f9fcf38800e464f88e189f462b
LORAN <vinana222@gmail.com>
2024-03-26 11:50:44 +0300

    19

---
+++ Homework/dz_6/dz_6/dz_6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+Написать программу, которая выводит на экран прямоугольник символом #. Высота и ширина задаётся пользователям. 
+*/
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int height, width;
+
+    cout << "Введите высоту прямоугольника" << endl;
+    cin >> height;
+
+    cout << "Введите ширину прямоугольника" << endl;
+    cin >> width;
+
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            cout << "#";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
