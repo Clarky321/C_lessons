e654d64e011378e946a4b63ef0b1ddaa81afc499
LORAN <vinana222@gmail.com>
2024-04-16 21:15:17 +0300

    dz-8

---
+++ Homework/dz_8/dz_8/dz_8.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+/* Найти самое маленькое число в массиве и вывести его на экран */
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	srand(time(NULL));
+
+	const int SIZE = 10;
+	int arr[SIZE];
+
+	bool alreadyThere;
+
+	for (int i = 0; i < SIZE; )
+	{
+		alreadyThere = false;
+
+		int newRandomValue = rand() % 20;
+
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == newRandomValue)
+			{
+				alreadyThere = true;
+				break;
+			}
+		}
+
+		if (!alreadyThere)
+		{
+			arr[i] = newRandomValue;
+			i++;
+		}
+	}
+	
+	int minValue = arr[0];
+
+	for (int i = 1; i < SIZE; i++)
+	{
+		if (arr[i] < minValue)
+		{
+			minValue = arr[i];
+		}
+	}
+
+	cout << "Наименьшее число в массиве: " << minValue << endl;
+
+	return 0;
+}
